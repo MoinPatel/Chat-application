cfeec797fe33cf1ed160821692a8f96d19b57731
Moin Patel <moinpatel1992@gmail.com>
2014-11-20 07:33:18 -0800

    server file updated

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var http = require('http');
 http = http.Server(app);
 var io = require('socket.io');
 io = io(http);
+var nicknames = [];
 
 // Route our Assets
 app.use('/assets/', express.static(__dirname + '/public/assets/'));
@@ -21,10 +22,29 @@ io.on('connection', function(socket){
   console.log('A User Connected');
 
   // Handle Message Event
-  socket.on('message', function(text){
-    io.emit('update', text);
+  socket.on('message', function(text, callback){
+  	if (nicknames.indexOf(text) != -1){
+			callback(false);
+		} else{
+			callback(true);
+			socket.nickname = text;
+			nicknames.push(socket.nickname);
+			updateNicknames();
+		}
+    
   });
-
+  function updateNicknames(){
+		io.sockets.emit('usernames', nicknames);
+	}
+  socket.on('send message', function(data){
+  		//io.emit('update', text);
+		io.sockets.emit('new message', {msg: text, nick: socket.nickname});
+	});
+  socket.on('disconnect', function(data){
+		if(!socket.nickname) return;
+		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		updateNicknames();
+	});
 });
 
 // Start Server
